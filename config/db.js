ae4734e281389f9c42c5f94cad8c38db08d94e71
vivekchauhan12000 <49834398+vivekchauhan12000@users.noreply.github.com>
2020-06-10 21:00:23 +0530

    optimized

--- config/db.js
+++ config/db.js
@@ -1,5 +1,5 @@
 const mongoose = require('mongoose');
-const MONGO_URI="mongodb+srv://admin:admin@cluster0.fhcsh.mongodb.net/locator?retryWrites=true&w=majority";
+const MONGO_URI="";
 
 const connectDB = async ()=>{
   try{
